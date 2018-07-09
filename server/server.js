1c4adee081a12f48b0cdf2a4a763fdac5e236c40
James Newman <jamesmnewman1@gmail.com>
2018-07-08 23:50:37 -0700

    adding routes, will add .env for db creds

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+// add db url
+
 app.use('/', router);
 
-module.exports=app;
+module.exports = app;
