9f7f182049421a0c3e1546c98dbc01d003d70854
James Newman <jamesmnewman1@gmail.com>
2018-07-13 23:59:49 -0700

    commit here

--- client/index.js
+++ client/index.js
@@ -1,4 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './components/App';
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// client/index.js - 7/13
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter } from 'react-router-dom'
+import Routes from './routes'
+
+ReactDOM.render(
+  <HashRouter>
+    <Routes />
+  </HashRouter>, document.getElementById('root')
+);
